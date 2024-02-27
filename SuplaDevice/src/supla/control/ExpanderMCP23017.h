1cc7c96111733fff14292aeca59c107e44a2e34e
glock4101 <97336775+glock4101@users.noreply.github.com>
2024-02-27 07:04:10 +0100

    Update ExpanderMCP23017.h

--- SuplaDevice/src/supla/control/ExpanderMCP23017.h
+++ SuplaDevice/src/supla/control/ExpanderMCP23017.h
@@ -14,6 +14,8 @@
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+ Bibliteka MCP https://github.com/adafruit/Adafruit-MCP23017-Arduino-Library
 */
 
 #ifndef EXPANDER_MCP23017_H_
