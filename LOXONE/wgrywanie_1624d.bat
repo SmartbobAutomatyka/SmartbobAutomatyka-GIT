fdbddcbdab35e09e20a174962d5ec22ab9b4262d
glock4101 <97336775+glock4101@users.noreply.github.com>
2025-10-26 22:48:56 +0100

    EXPANDER SM-EXP-16IN
    Dodanie obsługi expanderów SM-EXP-16IN do SM-LITE-1616R oraz SM-LITE1624D

--- LOXONE/wgrywanie_1624d.bat
+++ LOXONE/wgrywanie_1624d.bat
@@ -1,2 +1,2 @@
 esptool.exe -p COM4 erase_flash
-esptool.exe -p COM4 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 SM-LITE-1624D/SMARTBOBSOFT-SM-1624D.ino.bootloader.bin 0x8000 SM-LITE-1624D/SMARTBOBSOFT-SM-1624D.ino.partitions.bin 0x10000 SM-LITE-1624D/SMARTBOBSOFT-SM-1624D.ino.bin
+esptool.exe -p COM4 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 SM-LITE-1624D/SMARTBOBSOFT-SM-1624DV15.ino.bootloader.bin 0x8000 SM-LITE-1624D/SMARTBOBSOFT-SM-1624DV15.ino.partitions.bin 0x10000 SM-LITE-1624D/SMARTBOBSOFT-SM-1624DV15.ino.bin
