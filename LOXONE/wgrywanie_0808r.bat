3bf2fc218d7907aa8df37376c5f14513d03450c1
SmartbobAutomatyka <orders@smartbob.pl>
2026-02-06 19:33:28 +0100

    REMOVE LOGER

--- LOXONE/wgrywanie_0808r.bat
+++ LOXONE/wgrywanie_0808r.bat
@@ -1,2 +1,2 @@
-esptool.exe -p COM12 erase_flash
-esptool.exe -p COM12 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 SM-MINI-0808R/SMARTBOBSOFT-SM-0808R.ino.bootloader.bin 0x8000 SM-MINI-0808R/SMARTBOBSOFT-SM-0808R.ino.partitions.bin 0x10000 SM-MINI-0808R/SMARTBOBSOFT-SM-0808R.ino.bin
+esptool.exe -p COM4 erase_flash
+esptool.exe -p COM4 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 SM-MINI-0808R/SMARTBOBSOFT-SM-0808R.ino.bootloader.bin 0x8000 SM-MINI-0808R/SMARTBOBSOFT-SM-0808R.ino.partitions.bin 0x10000 SM-MINI-0808R/SMARTBOBSOFT-SM-0808R.ino.bin
--- LOXONE/wgrywanie_1616r.bat
+++ LOXONE/wgrywanie_1616r.bat
@@ -1,2 +1,2 @@
-esptool.exe -p COM14 erase_flash
-esptool.exe -p COM14 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 SM-LITE-1616R/SMARTBOBSOFT-SM-1616R.ino.bootloader.bin 0x8000 SM-LITE-1616R/SMARTBOBSOFT-SM-1616R.ino.partitions.bin 0x10000 SM-LITE-1616R/SMARTBOBSOFT-SM-1616R.ino.bin
+esptool.exe -p COM4 erase_flash
+esptool.exe -p COM4 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 SM-LITE-1616R/SMARTBOBSOFT-SM-1616R.ino.bootloader.bin 0x8000 SM-LITE-1616R/SMARTBOBSOFT-SM-1616R.ino.partitions.bin 0x10000 SM-LITE-1616R/SMARTBOBSOFT-SM-1616R.ino.bin
