83011998c258dc603e4f997bcf4af462a8f8b171
Krzysztof Bogus - SmartBob <smartbobautomatyka@gmail.com>
2026-04-22 17:02:05 +0200

    Sprzątanie

--- SUPLA/wgrywanie_1624d.bat
+++ SUPLA/wgrywanie_1624d.bat
@@ -1,2 +1,2 @@
-esptool.exe -p COM4 erase_flash
-esptool.exe -p COM4 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m  0x0 SM-LITE-1624D-hw15-v26.04.15-M.bin
+esptool.exe -p COM6 erase_flash
+esptool.exe -p COM6 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m  0x0 SM-LITE-1624D-hw15-v26.04.21-M.bin
--- SUPLA/wgrywanie_3236d.bat
+++ SUPLA/wgrywanie_3236d.bat
@@ -1,2 +1,2 @@
-esptool.exe -p COM4 erase_flash
-esptool.exe -p COM4 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m  0x0 SM-PRO-3236D-v26.04.15-M.bin
+esptool.exe -p COM13 erase_flash
+esptool.exe -p COM13 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m  0x0 SM-PRO-3236D-v26.04.15-M.bin
